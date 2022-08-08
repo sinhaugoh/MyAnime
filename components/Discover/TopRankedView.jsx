c3501d283ed8ef03c6a811c3c0cd6e8b8344ef70
sinhaugoh <sinhaugoh456@hotmail.com>
2022-08-09 01:14:50 +0800

    implemented to ranked view

--- components/Discover/TopRankedView.jsx
+++ components/Discover/TopRankedView.jsx
@@ -1,8 +1,83 @@
-import { View, Text } from "react-native";
+import { StyleSheet, Text, FlatList } from "react-native";
+import { useState, useEffect } from "react";
+import { useTheme } from "../../contexts/ThemeContext";
+import AnimeTile from "./AnimeTile";
+import LoadingIndicator from "../shared/LoadingIndicator";
+import { JikanApi } from "../../services/JikanApi";
+
 export default function TopRankedView() {
+  const { theme } = useTheme();
+  const themedStyle = styles(theme);
+  const [isLoading, setIsLoading] = useState(true);
+  const [data, setData] = useState([]);
+  const [error, setError] = useState(null);
+  const [page, setPage] = useState(1);
+  const [hasNextPage, setHasNextPage] = useState(true);
+
+  function fetchNextPage() {
+    setPage((prevPage) => prevPage + 1);
+  }
+
+  useEffect(() => {
+    if (hasNextPage) {
+      (async function () {
+        try {
+          const data = await JikanApi.fetchTopRankedAnime(page);
+          // const formattedData = data.data.map((anime) => {
+          //   return {
+          //     mal_id: anime.mal_id,
+          //     title: anime.title_english,
+          //     image_url: anime.images.jpg.image_url,
+          //   };
+          // });
+
+          // append the data into data list
+          setData((prevData) => [...prevData, ...data.data]);
+          setHasNextPage(data.pagination.has_next_page);
+        } catch (e) {
+          setError(e);
+        } finally {
+          setIsLoading(false);
+        }
+      })();
+    }
+  }, [page, hasNextPage]);
+
+  if (isLoading) return <LoadingIndicator />;
+
+  //TODO: implement error screen
+  if (error) return <Text>Error fetching data from the API</Text>;
+
   return (
-    <View>
-      <Text>Top ranked</Text>
-    </View>
+    <FlatList
+      data={data}
+      numColumns={3}
+      renderItem={({ item }) => (
+        <AnimeTile title={item.title} image_url={item.images.jpg.image_url} />
+      )}
+      style={themedStyle.container}
+      ListFooterComponent={
+        hasNextPage ? (
+          LoadingIndicator
+        ) : (
+          <Text style={themedStyle.theEndText}>You have reach the end</Text>
+        )
+      }
+      onEndReachedThreshold={0.2}
+      onEndReached={hasNextPage ? fetchNextPage : null}
+    />
   );
 }
+
+const styles = (theme) =>
+  StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: theme.primaryBackgroundColor,
+    },
+    theEndText: {
+      color: theme.primaryTextColor,
+      textAlign: "center",
+      marginVertical: 5,
+    },
+  });
