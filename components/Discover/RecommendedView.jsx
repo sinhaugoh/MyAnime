7ec0ce47724017a9c5daa4036ea554742dd4a924
sinhaugoh <sinhaugoh456@hotmail.com>
2022-08-09 17:06:40 +0800

    added props into AnimeTile to be displayed in detail screen

--- components/Discover/RecommendedView.jsx
+++ components/Discover/RecommendedView.jsx
@@ -23,13 +23,6 @@ export default function RecommendedView() {
       (async function () {
         try {
           const data = await JikanApi.fetchAiringAnime(page);
-          // const formattedData = data.data.map((anime) => {
-          //   return {
-          //     mal_id: anime.mal_id,
-          //     title: anime.title_english,
-          //     image_url: anime.images.jpg.image_url,
-          //   };
-          // });
 
           // append the data into data list
           setData((prevData) => [...prevData, ...data.data]);
@@ -53,7 +46,21 @@ export default function RecommendedView() {
       data={data}
       numColumns={3}
       renderItem={({ item }) => (
-        <AnimeTile title={item.title} image_url={item.images.jpg.image_url} />
+        <AnimeTile
+          mal_id={item.mal_id}
+          title={item.title}
+          japaneseTitle={item.title_japanese}
+          image_url={item.images.jpg.image_url}
+          type={item.type}
+          episodes={item.episodes}
+          year={item.year}
+          studios={item.studios}
+          ageRating={item.rating}
+          rating={item.score}
+          rank={item.rank}
+          genres={item.genres.map((genre) => genre.name)}
+          synopsis={item.synopsis}
+        />
       )}
       style={themedStyle.container}
       ListFooterComponent={
--- screens/Discover/AnimeDetailScreen.jsx
+++ screens/Discover/AnimeDetailScreen.jsx
@@ -1,8 +1,24 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet, ScrollView } from "react-native";
+import { useTheme } from "../../contexts/ThemeContext";
+
 export default function AnimeDetailScreen() {
+  const { theme } = useTheme();
+  const themedStyle = styles(theme);
+
   return (
-    <View>
-      <Text>Detail</Text>
+    <View style={themedStyle.container}>
+      <ScrollView>
+        <Text>Detail</Text>
+      </ScrollView>
     </View>
   );
 }
+
+const styles = (theme) =>
+  StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: theme.primaryBackgroundColor,
+      padding: 8,
+    },
+  });
