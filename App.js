34218d9dd0e2e3b80f2dbb57363da7606c089798
sinhaugoh <sinhaugoh456@hotmail.com>
2022-08-14 23:15:52 +0800

    disabled landscape mode

--- App.js
+++ App.js
@@ -3,8 +3,12 @@ import BottomTabBar from "./components/shared/BottomTabBar";
 import { ThemeProvider } from "./contexts/ThemeContext";
 import { FavouriteAnimesProvider } from "./contexts/FavouriteAnimesContext";
 import { SettingsProvider } from "./contexts/SettingsContext";
+import * as ScreenOrientation from "expo-screen-orientation";
 
 export default function App() {
+  // allow only portrait up mode
+  ScreenOrientation.lockAsync(ScreenOrientation.OrientationLock.PORTRAIT_UP);
+
   return (
     <ThemeProvider>
       <FavouriteAnimesProvider>
