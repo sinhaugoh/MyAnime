dd292631b4451dbfe265981ac25beff97c25ed52
sinhaugoh <sinhaugoh456@hotmail.com>
2022-08-15 10:55:13 +0800

    designed settings screen

--- screens/Settings/SettingsScreen.js
+++ screens/Settings/SettingsScreen.js
@@ -1,9 +1,21 @@
-import { View, Text, Button } from "react-native";
+import { View, Text, Button, ScrollView, StyleSheet } from "react-native";
+import ThemedView from "../../components/shared/ThemedView";
 import { useSettings } from "../../contexts/SettingsContext";
 import { useEffect } from "react";
+import { useTheme } from "../../contexts/ThemeContext";
+import { TableView, Section, Cell } from "react-native-tableview-simple";
 
 export default function SettingsScreen({ navigation }) {
   const { genreExcludesPreferences } = useSettings();
+  const { theme } = useTheme();
+  const themedStyles = styles(theme);
+
+  const cellProps = {
+    cellStyle: "basic",
+    accessory: "DisclosureIndicator",
+    backgroundColor: theme.primaryBackgroundColor,
+    titleTextColor: theme.primaryTextColor,
+  };
 
   // navigate to genre preference screen if first time launch app
   useEffect(() => {
@@ -16,12 +28,37 @@ export default function SettingsScreen({ navigation }) {
   }, []);
 
   return (
-    <View>
-      <Text>Settings</Text>
-      <Button
-        onPress={() => navigation.navigate("Genre preference")}
-        title="Genre preference"
-      />
-    </View>
+    <ScrollView style={themedStyles.scrollView}>
+      <TableView>
+        <Section header="Appearance">
+          <Cell {...cellProps} title="Theme" />
+        </Section>
+        <Section header="Preferences">
+          <Cell
+            {...cellProps}
+            title="Genre blacklist"
+            onPress={() => navigation.navigate("Genre preference")}
+          />
+        </Section>
+        <Section header="Others">
+          <Cell {...cellProps} title="Clear data" />
+        </Section>
+      </TableView>
+    </ScrollView>
   );
 }
+
+const styles = (theme) =>
+  StyleSheet.create({
+    scrollView: {
+      backgroundColor: theme.primaryBackgroundColor,
+      height: "100%",
+    },
+  });
+// <View>
+//   <Text>Settings</Text>
+//   <Button
+//     onPress={() => navigation.navigate("Genre preference")}
+//     title="Genre preference"
+//   />
+// </View>
